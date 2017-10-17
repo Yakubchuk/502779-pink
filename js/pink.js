bc5183295adbd00805e92fe527bd3c521327de7a
Yakubchuk <imy@inbox.ru>
2017-10-18 02:41:08 +0300

    Кнопка меню

--- js/pink.js
+++ js/pink.js
@@ -1,4 +1,30 @@
+$(document).ready(function () {
+  $(".main-nav__wrapper").toggleClass("main-nav__wrapper-open");
+  $(".header__list").toggleClass('toggle__menu');
+  if ($(".header__list").hasClass("toggle__menu"))
+    {
+      $(".button__img").show();
+      $(".button__img-open").hide();
+    }
+    else
+    {
+      $(".button__img").hide();
+      $(".button__img-open").show();
+    }
+});
 $("#menu_button").on("click",function () {
     $(".header__list").toggleClass('toggle__menu');
     $(".main-nav__wrapper").toggleClass('main-nav__wrapper-open');
+      if ($(".header__list").hasClass("toggle__menu"))
+        {
+          $(".button__img").show();
+          $(".button__img-open").hide();
+        }
+        else
+        {
+          $(".button__img").hide();
+          $(".button__img-open").show();
+        }
+
 });
+
