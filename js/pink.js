422a0d10fc4cc97043feb12a5de09eab1fa2e104
Yakubchuk <imy@inbox.ru>
2017-10-19 20:10:49 +0300

    новая навигация

--- js/pink.js
+++ js/pink.js
@@ -1,20 +1,20 @@
-$(document).ready(function () {
-  $(".main-nav__wrapper").toggleClass("main-nav__wrapper-open");
-  $(".header__list").toggleClass('toggle__menu');
-  if ($(".header__list").hasClass("toggle__menu"))
-    {
-      $(".button__img").show();
-      $(".button__img-open").hide();
-    }
-    else
-    {
-      $(".button__img").hide();
-      $(".button__img-open").show();
-    }
-});
+// $(document).ready(function () {
+//   $(".main-nav__wrapper").toggleClass("main-nav__wrapper-open");
+//   $(".header__list").toggleClass('toggle__menu');
+//   if ($(".header__list").hasClass("toggle__menu"))
+//     {
+//       $(".button__img").show();
+//       $(".button__img-open").hide();
+//     }
+//     else
+//     {
+//       $(".button__img").hide();
+//       $(".button__img-open").show();
+//     }
+// });
 $("#menu_button").on("click",function () {
     $(".header__list").toggleClass('toggle__menu');
-    $(".main-nav__wrapper").toggleClass('main-nav__wrapper-open');
+    $(".main-nav__wrapper--open").toggleClass('main-nav__wrapper--close');
       if ($(".header__list").hasClass("toggle__menu"))
         {
           $(".button__img").show();
@@ -25,6 +25,5 @@ $("#menu_button").on("click",function () {
           $(".button__img").hide();
           $(".button__img-open").show();
         }
-
 });
 
