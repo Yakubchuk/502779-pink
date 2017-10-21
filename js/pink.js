2f30611599d03a2c21649003c644b6b27f20010d
Yakubchuk <imy@inbox.ru>
2017-10-21 12:48:53 +0300

    update menu

--- js/pink.js
+++ js/pink.js
@@ -1,33 +1,4 @@
-// $(document).ready(function () {
-//   $(".main-nav__wrapper").toggleClass("main-nav__wrapper-open");
-//   $(".header__list").toggleClass('toggle__menu');
-//   if ($(".header__list").hasClass("toggle__menu"))
-//     {
-//       $(".button__img").show();
-//       $(".button__img-open").hide();
-//     }
-//     else
-//     {
-//       $(".button__img").hide();
-//       $(".button__img-open").show();
-//     }
-// });
-// $("#menu_button").on("click",function () {
-//     $(".header__list").toggleClass('toggle__menu');
-//     $(".main-nav__wrapper--open").toggleClass('main-nav__wrapper--close');
-//       if ($(".header__list").hasClass("toggle__menu"))
-//         {
-//           $(".button__img").show();
-//           $(".button__img-open").hide();
-//         }
-//         else
-//         {
-//           $(".button__img").hide();
-//           $(".button__img-open").show();
-//         }
-// });
 
-//дожидаемся полной загрузки страницы
 window.onload = function () {
 
     //получаем идентификатор элемента
